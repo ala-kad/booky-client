baf8f9b7e2dbc798f3aa10c8ef71ed824bf08b73
ala-kad <kaddechiala@gmail.com>
2025-03-12 23:28:09 +0100

    feat(api)!: Added BookyApiComponent  View

---
+++ src/views/BookyApi.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TableComponent from '../components/booky/Table';
+
+export const BookyApiComponent = () => {
+
+  return (
+    <>
+      <h2>Book List</h2>
+      <TableComponent />
+    </>
+  );
+}
+
+export default BookyApiComponent;
