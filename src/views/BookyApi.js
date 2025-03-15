252c0fac10079bf3ed7dd222a0b0dc829fc3412a
ala-kad <kaddechiala@gmail.com>
2025-03-15 23:28:04 +0100

    feat: rendered createBookComponent in BookyApi view

--- src/views/BookyApi.js
+++ src/views/BookyApi.js
@@ -1,11 +1,13 @@
 import React from "react";
 import TableComponent from '../components/booky/Table';
+import CreateBookComponent from '../components/booky/Create';
 
 export const BookyApiComponent = () => {
 
   return (
     <>
       <h2>Book List</h2>
+      <CreateBookComponent />
       <TableComponent />
     </>
   );
